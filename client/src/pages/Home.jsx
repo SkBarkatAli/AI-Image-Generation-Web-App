ff206452f735ccbd90319c20f1e9782ad284ae7a
SkBarkatAli <skbarkatali445@gmail.com>
2025-07-24 05:59:24 +0530

    create SearchBar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,49 @@
 import React from "react";
 import styled from 'styled-components'
+import SearchBar from "../components/SearchBar";
 
 const Container = styled.div`
    height:100%;
    overflow-y: scroll;
+   background: ${({theme})=>theme.bg};
+   padding:30px 30px;
+   padding-bottom: 50px;
+   display:flex;
+   flex-direction: column;
+   align-items: center;
+   gap: 20px;
+   @media(max-width:768px){
+   padding:6px 10px;}
+`;
+
+const Headline=styled.div`
+   font-size:34px;
+   font-weight:500;
+   color: ${({theme})=>theme.text_primary};
+   display: flex;
+   align-items: center;
+   flex-direction: column;
+
+   @media(max-width:600px){
+   font-size:22px
+   }
+`;
+const Span = styled.div`
+   font-size:30px;
+   font-weight:800;
+   color: ${({theme})=>theme.secondary};
 `;
 
 const Home = () => {
-    return <Container>Home</Container>;
+    return (
+         <Container>
+        <Headline>
+            Explore popular post in the Community!
+            <Span>⦿ Generated with AI ⦿</Span>
+        </Headline>
+        <SearchBar/>
+    </Container>
+    );
 };
 
 export default Home;
